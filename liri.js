98afae2669d7ffc2a1e75da13918ec7a6fba1327
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-09-23 20:16:51 -0700

    pseudo code for function spotify

--- liri.js
+++ liri.js
@@ -2,4 +2,19 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+// grab arguments in node terminal
+
+var nodeArgs = process.argv[2];
+
+// package for reading and writing files 
+var fs = require('fs');
+
+// function spotifyThis(nameOfSong){
+//   if (nameOfSong === undefined){
+//     nameOfSong === "The Sign";
+//   }
+
+
+console.log('is this working?');
