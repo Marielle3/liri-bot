5b83b7bbf8dc6f546a082c68e611453a57be1ece
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-09-25 10:19:32 -0700

    function for spotify this is still not working

--- liri.js
+++ liri.js
@@ -1,22 +1,69 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-
-var spotifyThis = require('spotify.js');
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+// var spotifyThis = require('spotify.js');
 // package for reading and writing files 
 var fs = require('fs');
 
 // grab arguments in node terminal
 //creates the user inputs to search for specific song 
-var userRequest = process.argv[2];
-var userInput = process.argv.splice(3).join(' ');
+var command = process.argv[2];
+var parameter = process.argv.splice(3).join(' ');
 
 
 console.log('is this working?');
 
+function switchCase (){
+  switch (command){
+    case 'spotify-this-song':
+    spotifySong(parameter);
+    break;
+    
+    case 'movie-this':
+    omdbInfo(parameter);
+    break
+  }
+};
+//SPOTIFY
+let spotifyThis = "Spotify"
 
-if (process.argv[2] === "spotify-this-song"){
-  var song = userInput
-  spotifyThis();
-}
-module.exports = spotifyThis
+function spotifySong(parameter) {
+
+
+  var searchSong;
+  if (!parameter) {
+    searchSong = "Ace of Base The Sign";
+  } else {
+    searchTrack = parameter;
+  }
+
+  Spotify(spotifyThis, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
+  spotify.search({
+    type: 'track',
+    query: searchSong
+  }, function(error, data) {
+    if (error) {
+      display('Error recorded: ' + error);
+      return;
+    } else {
+      console.log("\n---------------------------------------------------\n");
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Preview: " + data.tracks.items[3].preview_url);
+      console.log("Album: " + data.tracks.items[0].album.name);
+      console.log("\n---------------------------------------------------\n");
+      
+    }
+  
+  });
+};
--- spotify.js
+++ spotify.js
@@ -1,40 +1,83 @@
-// package for reading and writing files 
-var fs = require('fs');
+// // package for reading and writing files 
+// var fs = require('fs');
 
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
-var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
 
 
-function spotifyThis(userInput){
-  var nameOfSong = userInput;
-  if (!nameOfSong){
-    nameOfSong === "The Sign by Ace of Base";
-  }
-  spotify.search({
-    type: 'track',
-    query: 'song'
-  },
-  function(err, data){
-   if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-});
+// function spotifyThis(userInput){
+//   var nameOfSong = userInput;
+//   if (!nameOfSong){
+//     nameOfSong === "The Sign by Ace of Base";
+//   }
+//   spotify.search({
+//     type: 'track',
+//     query: 'song'
+//   },
+//   function(err, data){
+//    if ( err ) {
+//         console.log('Error occurred: ' + err);
+//         return;
+//     }
+// });
 
-  // Next, we append the text into the "random.txt" file to store all the song search
-// If the file didn't exist, then it gets created on the fly.
-fs.appendFile("random.txt", userInput, function(err) {
 
-  // If an error was experienced we will log it.
-  if (err) throw err; {
-    console.log("Another Song Saved");
-  }
+// //SPOTIFY
+// let spotifyFig = "Spotify"
 
-  // If no error is experienced, we'll log the phrase "Content Added" to our node console.
-  else {
-    console.log("Content Added!");
-  }
+// function spotifySong(parameter) {
 
-});
+
+//   var searchTrack;
+//   if (parameter === undefined) {
+//     searchTrack = "Ace of Base The Sign";
+//   } else {
+//     searchTrack = parameter;
+//   }
+
+//   figlet(spotifyFig, function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(chalk.green(data));
+// });
+
+//   spotify.search({
+//     type: 'track',
+//     query: searchTrack
+//   }, function(error, data) {
+//     if (error) {
+//       display('Error recorded: ' + error);
+//       return;
+//     } else {
+//       display(chalk.blue("\n---------------------------------------------------\n"));
+//       display(chalk.green("Artist: " + data.tracks.items[0].artists[0].name));
+//       display(chalk.green("Song: " + data.tracks.items[0].name));
+//       display(chalk.green("Preview: " + data.tracks.items[3].preview_url));
+//       display(chalk.green("Album: " + data.tracks.items[0].album.name));
+//       display(chalk.blue("\n---------------------------------------------------\n"));
+      
+//     }
+  
+//   });
+// };
+
+//   // Next, we append the text into the "random.txt" file to store all the song search
+// // If the file didn't exist, then it gets created on the fly.
+// fs.appendFile("random.txt", userInput, function(err) {
+
+//   // If an error was experienced we will log it.
+//   if (err) throw err; {
+//     console.log("Another Song Saved");
+//   }
+
+//   // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+//   else {
+//     console.log("Content Added!");
+//   }
+
+// });
